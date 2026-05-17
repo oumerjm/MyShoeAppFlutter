209b47402f43e52fde20a71bab37ab51476ae9ff
Oumer Jemal <oumerjemal293@gmail.com>
2026-05-17 18:34:49 +0300

    done the app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shoe_app_flutter/constants.dart';
 
 void main() {
   runApp(const ShoeApp());
 }
-
 
 class ShoeApp extends StatelessWidget {
   const ShoeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: "Shoe_App",
-      debugShowCheckedModeBanner: false ,
-      theme: ThemeData.dark(
-        useMaterial3: true
-      ),
-      home:  ProductDetailPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: ProductDetailPage(),
     );
   }
 }
@@ -31,36 +30,104 @@ class ProductDetailPage extends StatefulWidget {
 class _ProductDetailPageState extends State<ProductDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
-        children: [
-          Placeholder(fallbackHeight: 80,),
-          Expanded(
-            
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //top navbar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Placeholder(),
-                  SizedBox(height: 20,),
-                  Column(
+                  GestureDetector(
+                    onTap: () {
+                      print("ouch!!");
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: kDark,
+                      size: 20,
+                    ),
+                  ),
+                  //try to get the image in a local folder later
+                  SvgPicture.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg',
+                    height: 22,
+                    colorFilter: const ColorFilter.mode(kDark, BlendMode.srcIn),
+                  ),
+                  Stack(
+                    clipBehavior: Clip.none,
                     children: [
-                      Placeholder(),
-                      SizedBox(height: 20,),
-                      Placeholder(),
-                      SizedBox(height: 20,),
-                      Placeholder()
+                      Container(
+                        width: 42,
+                        height: 42,
+                        decoration: BoxDecoration(
+                          color: kDark,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Icon(
+                          Icons.shopping_bag_rounded,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                      Positioned(
+                        top: -4,
+                        right: -4,
+
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: kOrange,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Text(
+                            "3",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
-                  )
+                  ),
                 ],
               ),
-            )),
-          Expanded(child: Placeholder(fallbackHeight: 70,))
-        ],  
-      )),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Placeholder(fallbackHeight: 260, color: Colors.blue),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          Placeholder(fallbackHeight: 70),
+                          SizedBox(height: 20),
+                          Placeholder(fallbackHeight: 90),
+                          SizedBox(height: 20),
+                          Placeholder(fallbackHeight: 120),
+                          SizedBox(height: 100),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Placeholder(fallbackHeight: 70),
+          ],
+        ),
+      ),
     );
   }
 }
